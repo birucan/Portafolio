6b80cc3c544b38e802d87042840e3d5e4dcef473
birucan <t.kavanagh@uniandes.edu.co>
2020-01-27 18:06:47 -0500

    small edit

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -31,25 +31,26 @@ function Links() {
       );
 }else{
   return (
-    <div className="mLinks">
-    <h1>Links</h1>
-    <mcvLink>
-    <h1>Github</h1>
-    <mtextCV>This is my github profile, you can check out my proyects in here, more to come soon.</mtextCV>
-    <a  href='http://www.github.com/birucan' >
+    <div className="mProjects">
+    <h1>Current Projects</h1>
+    <h4>I'm showcasing my most recent projects here, I'll constantly update this section</h4>
+    <cvLink>
+    <h1>Portafolio Site</h1>
+    <textCV>This is this site! Made in react and will be updated consistently</textCV>
+    <a  href='https://github.com/birucan/Portafolio' >
     <img src={github}className="pdf"/>
     </a>
-    </mcvLink>
+    </cvLink>
 
       <space/>
 
-    <mcvLink>
-    <h1>Email</h1>
-    <mtextCV>If you need to contact me, you can email me at <b>t.kavanagh@uniandes.edu.co</b></mtextCV>
-    <a  href='mailto:t.kavanagh@uniandes.edu.co' >
+    <cvLink>
+    <h1>Javamon</h1>
+    <textCV>a very OOP aproach to a pokemon engine built in java, still a WIP</textCV>
+    <a  href='https://github.com/birucan/JavaMon' >
     <img src={email}className="pdf"/>
     </a>
-    </mcvLink>
+    </cvLink>
     </div>
   );
 
